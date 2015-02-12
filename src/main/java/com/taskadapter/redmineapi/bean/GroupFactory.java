0f9b8e893e6b9b37802668e99b9917c118063c57
Alexey Skorokhodov <alskor@gmail.com>
2015-02-11 19:06:17 -0800

    fixed constructor call in GroupFactory

--- src/main/java/com/taskadapter/redmineapi/bean/GroupFactory.java
+++ src/main/java/com/taskadapter/redmineapi/bean/GroupFactory.java
@@ -5,7 +5,7 @@ public class GroupFactory {
      * For new objects not added to Redmine yet. the ID field will be initialized to NULL.
      */
     public static Group create() {
-        return new Group(null);
+        return new Group(new PropertyStorage());
     }
 
     public static Group create(Integer id) {
